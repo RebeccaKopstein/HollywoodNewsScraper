ff32df0a2a263ac14463867770d226d4380ada84
Becca K <rkopstein@gmail.com>
2018-07-30 23:48:06 -0400

    added to all

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,9 +1,8 @@
-// Scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
-// Requiring our Comment and Article models
-var Comment = require("../models/Comment.js");
-var Article = require("../models/Article.js");
+
+var Comment = require("../models/Note.js");
+var Article = require("../models/article.js");
 
 module.exports = function (app) {
 
@@ -13,13 +12,12 @@ module.exports = function (app) {
         });
 
     app.get("/scrape", function (req, res) {
-        //use request dependecy to grab the body of the html
-        request("https://petapixel.com/", function (error, response, html) {
-            //Save the body of the html into a variabl called $  within cheerio
+
+        request("https://eonline.com/news", function (error, response, html) {
+
             var $ = cheerio.load(html);
-            // Now grab every a tag link within an article heading  and iterate through it
-            // and perform the following
-            $(".post-excerpt").each(function (i, element) {
+
+            $(".category-landing").each(function (i, element) {
 
                 var title = $(this)
                     .children("h2")
@@ -34,20 +32,19 @@ module.exports = function (app) {
                     .text();
 
                 if (title && link && articleSnippet) {
-                    // Save an empty result object
+
                     var result = {};
 
-                    // Add the text and href of every link, and save them as properties of the
-                    // result object
+
                     result.title = title;
                     result.link = link;
                     result.articleSnippet = articleSnippet;
 
-                    // Using our Article model, create a new entry
+
                     Article.create(result, function (err, doc) {
-                        // Log any errors
+
                         if (err) {
-                            console.log(err// Or log the doc
+                            console.log(err
                             );
                         } else {
                             console.log(doc);
@@ -56,58 +53,57 @@ module.exports = function (app) {
                 }
             });
         });
-        // Tell the browser that we finished scraping the text
+
         res.redirect("/");
     });
 
-    // This will get the articles we scraped from the mongoDB
+
     app.get("/articles", function (req, res) {
-        // Grab every doc in the Articles array
+
         Article
             .find({}, function (error, doc) {
-                // Log any errors
+
                 if (error) {
-                    console.log(error// Or send the doc to the browser as a json object
+                    console.log(error
                     );
                 } else {
                     res.render("index", { result: doc });
                 }
-                //Will sort the articles by most recent (-1 = descending order)
+
             })
             .sort({ '_id': -1 });
     });
 
-    // Grab an article by it's ObjectId
+
     app.get("/articles/:id", function (req, res) {
-        // Using the id passed in the id parameter, prepare a query that finds the
-        // matching one in our db...
+
         Article.findOne({ "_id": req.params.id })
-            // ..and populate all of the comments associated with it
+
             .populate("comment")
-            // now, execute our query
+
             .exec(function (error, doc) {
-                // Log any errors
+
                 if (error) {
-                    console.log(error// Otherwise, send the doc to the browser as a json object
+                    console.log(error
                     );
                 } else {
                     res.render("comments", { result: doc });
-                    // res.json (doc);
+
                 }
             });
     });
 
-    // Create a new comment
+
     app.post("/articles/:id", function (req, res) {
-        // Create a new Comment and pass the req.body to the entry
+
         Comment
             .create(req.body, function (error, doc) {
-                // Log any errors
+
                 if (error) {
-                    console.log(error// Otherwise
+                    console.log(error
                     );
                 } else {
-                    // Use the article id to find and update it's comment
+
                     Article.findOneAndUpdate({
                         "_id": req.params.id
                     }, {
@@ -119,13 +115,13 @@ module.exports = function (app) {
                             upsert: true,
                             new: true
                         })
-                        // Execute the above query
+
                         .exec(function (err, doc) {
-                            // Log any errors
+
                             if (err) {
                                 console.log(err);
                             } else {
-                                // Or send the document to the browser
+
                                 res.redirect('back');
                             }
                         });
@@ -136,9 +132,9 @@ module.exports = function (app) {
     app.delete("/articles/:id/:commentid", function (req, res) {
         Comment
             .findByIdAndRemove(req.params.commentid, function (error, doc) {
-                // Log any errors
+
                 if (error) {
-                    console.log(error// Otherwise
+                    console.log(error
                     );
                 } else {
                     console.log(doc);
@@ -149,9 +145,9 @@ module.exports = function (app) {
                                 "comment": doc._id
                             }
                         })
-                        // Execute the above query
+
                         .exec(function (err, doc) {
-                            // Log any errors
+
                             if (err) {
                                 console.log(err);
                             }
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,7 @@
-// After the dom loads, if the delete button is clicked run a delete method on the comment and clear the comment fields. refreshes page.
 $(document).ready(function () {
     $('.delete-button').on('click', function (e) {
         e.preventDefault();
-        var qURL = location.href + '/' + $(this).data('comment');
+        var qURL = location.href + '/' + $(this).data('note');
         $.ajax({
             method: "DELETE",
             url: qURL
